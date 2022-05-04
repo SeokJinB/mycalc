a1020be9163909f0c771b252399699c4cdfdc4cb
SeokJinB <100738541+SeokJinB@users.noreply.github.com>
2022-05-04 09:26:34 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data=True,
+  name = 'mycalc0001',
+  version  '0.0.1',
+  description='oss-dev my calculator 0001',
+  author = 'seokjinbae',
+  autor_email='1923926@donga.ac.kr',
+  url="https://github.com/SeokJinB/mycalc/new/main",
+  downloadurl = "https://github.com/SeokJinB/mycalc/new/main",
+  install_reqires=['pytest'],
+  long_description='oss-dev calculator python module',
+  long_description_content_type='text/markdown';,
+  calssifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
